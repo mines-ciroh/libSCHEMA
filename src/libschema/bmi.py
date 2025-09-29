008fb113733bde4b0a992b2e50edf1c45a99907f
Daniel Philippus <dphilippus@mines.edu>
2025-09-29 12:58:21 -0600

    Update bmi.py (remove excess logging)

--- src/libschema/bmi.py
+++ src/libschema/bmi.py
@@ -65,14 +65,16 @@ class SchemaBmi(Bmi):
     def update(self):
         # self._model.step()
         try:
-            self._model.log("Attempting value updates")
+            # self._model.log("Attempting value updates")
             for k in self._values:
                 self._model.set_val(self._input_map[k], self._values[k])
-            self._model.log("Values updated")
+            # self._model.log("Values updated")
             self._timestep += 3600
             if self._timestep % 86400 < 1:
                 self._values[self._output_var_name][0] = self._model.run_step()
-            self._model.log("Model step executed")
+                self.vlists[self._output_var_name][0] = self._values[self._output_var_name][0]
+                self._model.log("Model day executed")
+            # self._model.log("Model step executed")
             # for k in self._values:
             #     if k != self._output_var_name:
             #         self._values[k][0] = self._model.values[self._input_map[k]]
@@ -89,87 +91,87 @@ class SchemaBmi(Bmi):
         self._model = None
     
     def get_component_name(self):
-        self._model.log("get_component_name")
+        # self._model.log("get_component_name")
         return self._name
     
     def get_input_item_count(self):
-        self._model.log("get_input_item_count")
+        # self._model.log("get_input_item_count")
         return len(self._input_var_names)
     
     def get_output_item_count(self):
-        self._model.log("get_output_item_count")
+        # self._model.log("get_output_item_count")
         return len(self._output_var_names)
     
     def get_input_var_names(self):
-        self._model.log("get_input_var_names")
+        # self._model.log("get_input_var_names")
         return self._input_var_names
     
     def get_output_var_names(self):
-        self._model.log("get_output_var_names")
+        # self._model.log("get_output_var_names")
         return self._output_var_names
     
     def get_var_grid(self, name):
-        self._model.log("get_var_grid")
+        # self._model.log("get_var_grid")
         return 0
     
     def get_var_type(self, name):
-        self._model.log("get_var_type")
+        # self._model.log("get_var_type")
         return "float"
     
     def get_var_units(self, name):
-        self._model.log("get_var_units")
+        # self._model.log("get_var_units")
         return self._var_units[name]
     
     def get_var_itemsize(self, name):
-        self._model.log("get_var_itemsize")
+        # self._model.log("get_var_itemsize")
         return 8
     
     def get_var_nbytes(self, name):
-        self._model.log("get_var_nbytes")
+        # self._model.log("get_var_nbytes")
         try:
             return self.get_value_ptr(name).nbytes
         except Exception as e:
             self._model.log(f"Error in get_value_ptr: {e}")
     
     def get_var_location(self, name):
-        self._model.log("get_var_location")
+        # self._model.log("get_var_location")
         return "node"
     
     def get_current_time(self):
-        self._model.log("get_current_time")
+        # self._model.log("get_current_time")
         return self._timestep
     
     def get_start_time(self):
-        self._model.log("get_start_time")
+        # self._model.log("get_start_time")
         return 0.0
     
     def get_end_time(self):
-        self._model.log("get_end_time")
+        # self._model.log("get_end_time")
         return self._end_time
     
     def get_time_units(self):
-        self._model.log("get_time_units")
+        # self._model.log("get_time_units")
         return self._time_units
     
     def get_time_step(self):
-        self._model.log("get_time_step")
+        # self._model.log("get_time_step")
         return 3600.0
     
     def get_value_ptr(self, name):
-        self._model.log("get_value_ptr")
+        # self._model.log("get_value_ptr")
         return self._values[name]
     
     def get_value(self, name, dest):
-        self._model.log("get_value")
+        # self._model.log("get_value")
         dest[:] = np.array(self.get_value_ptr(name))
         return dest
     
     def get_value_at_indices(self, name, dest, inds):
-        self._model.log("get_value_at_indices")
+        # self._model.log("get_value_at_indices")
         return self.get_value(name, dest)
     
     def set_value(self, name, src):
-        self._model.log(f"Attempting value set: {name} = {src}")
+        # self._model.log(f"Attempting value set: {name} = {src}")
         val = src[0]
         self._values[name][0] = val
         self.vlists[name].append(val)
@@ -180,7 +182,7 @@ class SchemaBmi(Bmi):
         self.set_value(name, src)
         
     def get_grid_type(self, grid):
-        self._model.log("get_grid_type")
+        # self._model.log("get_grid_type")
         return "scalar"
     
     def get_grid_rank(self, grid):
